aa74d5aa8158aa7676f5b818c1cac83474a4b061
Dbirtic <Dominik.birtic666@gmail.com>
2022-04-26 07:46:49 +0200

    added an example for tkinter

--- main_tkinter.py
+++ main_tkinter.py
@@ -1,9 +1,10 @@
 from ctypes.wintypes import WORD
 import tkinter
-from tkinter import END, Button, Entry, ttk
+from tkinter import END, VERTICAL, Button, Entry, ttk
+from tkinter.messagebox import showinfo
 
 # key down function
-def click():
+"""def click():
     entered_text = textentry.get() # this will collect the text from the textbox
     output.delete(0.0, END)
     try:
@@ -11,6 +12,7 @@ def click():
     except:
         definition = "sorry, there is no word like that please try again"
     output.insert(END, definition)
+"""
 window = tkinter.Tk()
 window.title("Window")
 
@@ -21,21 +23,57 @@ frm = ttk.Frame(window, padding=10)
 frm.grid()
 
 # create label
-ttk.Label(frm, text="Hi man!", font="none 12").grid(row=0, column=0, sticky=tkinter.E)
-ttk.Button(frm, text="Quit", command=window.destroy).grid(column=0, row=3)
+#ttk.Label(frm, text="Hi man!", font="none 12").grid(row=0, column=0, sticky=tkinter.E)
+#ttk.Button(frm, text="Quit", command=window.destroy).grid(column=0, row=3)
 # create a text entry box
-textentry = Entry(frm, width=20, bg="white")
-textentry.grid(row=2, column=0, sticky=tkinter.W)
+#textentry = Entry(frm, width=20, bg="white")
+#textentry.grid(row=2, column=0, sticky=tkinter.W)
 
 # add a submit button
-Button(window, text="SUBMIT", width=6, command=click).grid(row=3, column=0)
+#Button(window, text="SUBMIT", width=6, command=click).grid(row=3, column=0)
 
 # create another label
-ttk.Label(frm, text="\nDefinition:", font="none 12 bold").grid(row=4, column=0, sticky=tkinter.W)
+#ttk.Label(frm, text="\nDefinition:", font="none 12 bold").grid(row=4, column=0, sticky=tkinter.W)
+
+# define columns
+columns = ('first_name', 'last_name', 'email')
+
+tree = ttk.Treeview(frm, columns=columns, show='headings')
+
+# define headings
+tree.heading('first_name', text='First Name')
+tree.heading('last_name', text='Last Name')
+tree.heading('email', text='Email')
+
+# generate sample data
+contacts = []
+for n in range(1, 100):
+    contacts.append((f'first {n}', f'last {n}', f'email{n}@example.com'))
+
+# add data to the treeview
+for contact in contacts:
+    tree.insert('', END, values=contact)
+
+def item_selected(event):
+    for selected_item in tree.selection():
+        item = tree.item(selected_item)
+        record = item['values']
+        # show a message
+        showinfo(title='Information', message=','.join(record))
+
+
+tree.bind('<<TreeviewSelect>>', item_selected)
+
+tree.grid(row=0, column=0, sticky='nsew')
 
 # create a text box
-output = tkinter.Text(frm, width=50, height=6, wrap="word")
-output.grid(row=5, column=0, columnspan=2, sticky=tkinter.W)
+#output = tkinter.Text(frm, width=50, height=6, wrap="word")
+#output.grid(row=5, column=0, columnspan=2, sticky=tkinter.W)
+
+# add a scrollbar
+scrollbar = ttk.Scrollbar(frm, orient=VERTICAL, command=tree.yview)
+tree.configure(yscroll=scrollbar.set)
+scrollbar.grid(row=0, column=1, sticky='ns')
 
 # the dictionary
 my_dict = {'algorithm': 'Step by step instructions to complete a task', 'bug': 'piece of code that causes program to fail'}
---
+++ tkinter_example.py
@@ -0,0 +1,45 @@
+from tkinter import *
+from tkinter import ttk
+
+# function which is called to calculate
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
+
+# instantiation of tkinter
+root = Tk()
+root.title("Feet to Meters")
+
+# frame config
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+# entry widget where feet will be entered
+feet = StringVar()
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+# label for meters
+meters = StringVar()
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+# adds a little bit of padding on each widget
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+# if user presses the Return key it should press the calculate button
+root.bind("<Return>", calculate)
+
+root.mainloop()
